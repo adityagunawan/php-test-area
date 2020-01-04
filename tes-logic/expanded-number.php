47e530f60d7c76e407331946bf233065f7d6e948
adityagunawan <aditya.gunawan1994@gmail.com>
2020-01-05 00:29:38 +0700

    number expanded-form

---
+++ tes-logic/expanded-number.php
@@ -0,0 +1,16 @@
+<?php
+
+function expanded_form($n) {
+    $rtn = [];
+    $count = strlen($n);
+    foreach(str_split($n) as $x) {
+      if ($x != 0) {
+          $rtn[] = str_pad($x, $count, 0);
+      }
+      $count--;
+    }
+    return implode($rtn, " + ");
+  }
+
+  print_r(expanded_form(2020)); // result 2000 + 20
+  print_r(expanded_form(1994)); // result 1000 + 900 + 90 + 4
